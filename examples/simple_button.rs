a7389d55d45f8b7e0a28874f56df6070d69bf347
Jovansonlee Cesar <ivanceras@gmail.com>
2020-04-18 18:55:31 +0800

    long button

--- examples/simple_button.rs
+++ examples/simple_button.rs
@@ -77,26 +77,26 @@ where
 
     let mut text_area1: TextArea<MSG> = TextArea::new(
         "This is a text area\
-            \n1. With a line\
+            \n1. With a line that is a bit long.. but not very long....\
             \n2. and another line\
             \n3. With a veryyyyyyy looooooooooooooooooooooooooooooooooooonnnnnnnnnnngggggggg line\
-            \n4. and another line\
+            \n4. and another line...............\
             \n5. With a line\
-            \n6. With a line\
+            \n6. With a line and not too long\
             \n7. and another line\
             \n8. With a line\
             \n9. and another line\
-            \n10. and another line\
+            \n10. and another line with another pharse\
             \n11. With a line\
             \n12. and another line\
             \n13. With a line\
-            \n14. and another line\
+            \n14. and another line not so loooooooooooooooooooooooooong\
             \n15. With a line\
             \n16. With a line\
             \n17. and another line\
             \n18. With a line\
             \n19. and another line\
-            \n",
+            \n20. This is the last line and also a looooooooooooooooooooong line",
     );
     text_area1.set_size(Some(40.0), Some(7.0));
 
