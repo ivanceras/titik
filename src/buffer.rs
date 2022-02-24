3e4fd1fbf6d4003417776963b2b14ec7332207bc
Jovansonlee Cesar <ivanceras@gmail.com>
2022-02-25 01:49:18 +0800

    fix: display back the colors of cell colors and attributes

--- src/buffer.rs
+++ src/buffer.rs
@@ -174,10 +174,23 @@ impl Buffer {
         for (j, line) in self.cells.iter().enumerate() {
             for (i, cell) in line.iter().enumerate() {
                 crossterm::queue!(w, cursor::MoveTo(i as u16, j as u16))?;
+                if let Some(bg) = cell.background_color {
+                    queue!(w, SetBackgroundColor(bg))
+                        .map_err(|_| fmt::Error)?;
+                }
+                if let Some(fg) = cell.foreground_color {
+                    queue!(w, SetForegroundColor(fg))
+                        .map_err(|_| fmt::Error)?;
+                }
+                if !cell.attributes.is_empty() {
+                    queue!(w, SetAttributes(cell.attributes))
+                        .map_err(|_| fmt::Error)?;
+                }
                 // fillter is \0 null character, filler is not printable
                 if !cell.is_filler() {
                     crossterm::queue!(w, Print(cell))?;
                 }
+                queue!(w, ResetColor).map_err(|_| fmt::Error)?;
             }
         }
         Ok(())
