c64bbc2191ae2376f330a104402372a27b930b03
Jovansonlee Cesar <ivanceras@gmail.com>
2022-02-26 05:43:27 +0800

    add guards to not process the events if it is just a mouse move

--- src/renderer.rs
+++ src/renderer.rs
@@ -70,6 +70,22 @@ impl<'a, MSG> Renderer<'a, MSG> {
         self.recompute_layout();
     }
 
+    #[cfg(feature = "crossterm_new")]
+    fn is_mouse_move(c_event: crate::crossterm::event::Event) -> bool {
+        match c_event {
+            crate::crossterm::event::Event::Mouse(me) => match me.kind {
+                crate::crossterm::event::MouseEventKind::Moved => true,
+                _ => false,
+            },
+            _ => false,
+        }
+    }
+
+    #[cfg(feature = "crossterm")]
+    fn is_mouse_move(_c_event: crate::crossterm::event::Event) -> bool {
+        false
+    }
+
     /// run the event loop of the renderer
     pub fn run(&mut self) -> Result<()> {
         command::init(&mut self.write)?;
@@ -89,79 +105,32 @@ impl<'a, MSG> Renderer<'a, MSG> {
             self.write.flush()?;
 
             if let Ok(c_event) = event::read() {
-                let event = Event::from_crossterm(c_event);
-                match event {
-                    Event::Key(key_event) => {
-                        // To quite, press any of the following:
-                        //  - CTRL-c
-                        //  - CTRL-q
-                        //  - CTRL-d
-                        //  - CTRL-z
-                        if key_event.modifiers.contains(KeyModifiers::CONTROL) {
-                            match key_event.code {
-                                KeyCode::Char(c) => match c {
-                                    'c' | 'q' | 'd' | 'z' => {
-                                        break;
-                                    }
-                                    _ => (),
-                                },
-                                _ => (),
-                            }
-                        } else {
-                            // send the keypresses to the focused widget
-                            if let Some(idx) = self.focused_widget_idx.as_ref()
-                            {
-                                let active_widget: Option<
-                                    &mut dyn Widget<MSG>,
-                                > = find_node::find_widget_mut(
+                if !Self::is_mouse_move(c_event) {
+                    let event = Event::from_crossterm(c_event);
+                    let should_break = self.process_event(&event);
+                    if should_break {
+                        break;
+                    }
+                    // any other activities, such as mouse scroll is
+                    // sent the widget underneath the location, regardless
+                    // if it focused or not.
+
+                    if let Some((x, y)) = event.extract_location() {
+                        let hits = self
+                            .root_node
+                            .node_hit_at(x as f32, y as f32, &mut 0);
+                        for hit in hits.iter().rev() {
+                            let mut hit_widget: Option<&mut dyn Widget<MSG>> =
+                                find_node::find_widget_mut(
                                     self.root_node,
-                                    *idx,
+                                    *hit,
                                 );
-                                if let Some(focused_widget) = active_widget {
-                                    let msgs = focused_widget
-                                        .process_event(event.clone());
-                                    self.dispatch_msg(msgs);
-                                }
+
+                            if let Some(hit_widget) = &mut hit_widget {
+                                let msgs =
+                                    hit_widget.process_event(event.clone());
+                                self.dispatch_msg(msgs);
                             }
-                        }
-                    }
-                    // mouse clicks sets the focused the widget underneath
-                    Event::Mouse(_me) => {
-                        if event.is_mouse_click() {
-                            let (x, y) = event
-                                .extract_location()
-                                .expect("must have a mouse location");
-                            self.focused_widget_idx = self
-                                .root_node
-                                .node_hit_at(x as f32, y as f32, &mut 0)
-                                .pop();
-
-                            if let Some(idx) = self.focused_widget_idx.as_ref()
-                            {
-                                self.root_node.set_focused_node(*idx);
-                            }
-                        }
-                    }
-                    Event::Resize(width, height) => {
-                        self.terminal_size = (width, height);
-                        self.recompute_layout();
-                    }
-                    _ => (),
-                }
-                // any other activities, such as mouse scroll is
-                // sent the widget underneath the location, regardless
-                // if it focused or not.
-
-                if let Some((x, y)) = event.extract_location() {
-                    let hits =
-                        self.root_node.node_hit_at(x as f32, y as f32, &mut 0);
-                    for hit in hits.iter().rev() {
-                        let mut hit_widget: Option<&mut dyn Widget<MSG>> =
-                            find_node::find_widget_mut(self.root_node, *hit);
-
-                        if let Some(hit_widget) = &mut hit_widget {
-                            let msgs = hit_widget.process_event(event.clone());
-                            self.dispatch_msg(msgs);
                         }
                     }
                 }
@@ -169,5 +138,62 @@ impl<'a, MSG> Renderer<'a, MSG> {
         }
         command::finalize(&mut self.write)?;
         Ok(())
+    }
+
+    /// process the event
+    fn process_event(&mut self, event: &Event) -> bool {
+        match event {
+            Event::Key(key_event) => {
+                // To quite, press any of the following:
+                //  - CTRL-c
+                //  - CTRL-q
+                //  - CTRL-d
+                //  - CTRL-z
+                if key_event.modifiers.contains(KeyModifiers::CONTROL) {
+                    match key_event.code {
+                        KeyCode::Char(c) => match c {
+                            'c' | 'q' | 'd' | 'z' => {
+                                return true;
+                            }
+                            _ => (),
+                        },
+                        _ => (),
+                    }
+                } else {
+                    // send the keypresses to the focused widget
+                    if let Some(idx) = self.focused_widget_idx.as_ref() {
+                        let active_widget: Option<&mut dyn Widget<MSG>> =
+                            find_node::find_widget_mut(self.root_node, *idx);
+                        if let Some(focused_widget) = active_widget {
+                            let msgs =
+                                focused_widget.process_event(event.clone());
+                            self.dispatch_msg(msgs);
+                        }
+                    }
+                }
+            }
+            // mouse clicks sets the focused the widget underneath
+            Event::Mouse(_me) => {
+                if event.is_mouse_click() {
+                    let (x, y) = event
+                        .extract_location()
+                        .expect("must have a mouse location");
+                    self.focused_widget_idx = self
+                        .root_node
+                        .node_hit_at(x as f32, y as f32, &mut 0)
+                        .pop();
+
+                    if let Some(idx) = self.focused_widget_idx.as_ref() {
+                        self.root_node.set_focused_node(*idx);
+                    }
+                }
+            }
+            Event::Resize(width, height) => {
+                self.terminal_size = (*width, *height);
+                self.recompute_layout();
+            }
+            _ => (),
+        }
+        false
     }
 }
